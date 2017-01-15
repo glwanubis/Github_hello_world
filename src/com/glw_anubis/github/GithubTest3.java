909986348b5c30bcd4479b715c6e0ea655203663
glwanubis <glw_anubis@163.com>
2017-01-15 16:37:08 +0800

    另一客户端的测试github的命名空间删掉了。郁闷，在修改看看能不能同步更新。

--- src/com/glw_anubis/github/GithubTest3.java
+++ src/com/glw_anubis/github/GithubTest3.java
@@ -5,6 +5,7 @@ public class GithubTest3 {
 	public static void main(String[] args) {
 
 		System.out.println("第一次上传的时候不能同步，删除后再导入一次试试");
+		System.out.println("另一客户端命名空间删掉了，郁闷，再来一次，看看能不能更新代码");
 	}
 
 }
